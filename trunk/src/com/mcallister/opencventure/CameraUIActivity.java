07e82d3c78524f59aeb503077771752c9b5d75c5
cjm571@gmail.com <cjm571@gmail.com@c399bc76-a552-f8f4-f1ce-ebdec458d828>
2012-04-02 04:22:17 +0000

    Added CameraPreview

--- trunk/src/com/mcallister/opencventure/CameraUIActivity.java
+++ trunk/src/com/mcallister/opencventure/CameraUIActivity.java
@@ -1,6 +1,7 @@
 package com.mcallister.opencventure;
 
 import android.app.Activity;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -11,9 +12,27 @@ public class CameraUIActivity extends Activity implements OnClickListener{
 	public void onCreate( Bundle savedInstanceState ){
     	super.onCreate( savedInstanceState );
         setContentView(R.layout.camera_ui);
+        
+        Camera cam = getCameraInstance();
+        
+        
     }
     
     public void onClick(View view){
     	System.out.println("Click Detected");
+    }
+    
+    /** safely access an instance of the camera */
+    public static Camera getCameraInstance(){
+    	Camera cam = null;
+    	
+    	try{
+    		cam = Camera.open();
+    	}
+    	catch ( Exception e ){
+    		System.out.println("No camera found");
+    	}
+    	
+    	return cam;
     }
 }

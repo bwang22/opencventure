44c0c0a5f7bf956ce6b22c20cd3924846580d5b9
cjm571@gmail.com <cjm571@gmail.com@c399bc76-a552-f8f4-f1ce-ebdec458d828>
2012-04-02 17:08:19 +0000

    Basic camera functionality implemented

--- trunk/src/com/mcallister/opencventure/CameraUIActivity.java
+++ trunk/src/com/mcallister/opencventure/CameraUIActivity.java
@@ -5,21 +5,116 @@ import android.hardware.Camera;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.FrameLayout;
 
 public class CameraUIActivity extends Activity implements OnClickListener{
-	/** Called when the activity is first created. */
+	private Camera mCamera = null;
+	private CameraPreview camPreview = null;
+	private FrameLayout previewFrame = null;
+	
+	/** Initializes camera and begins preview */
     @Override
 	public void onCreate( Bundle savedInstanceState ){
     	super.onCreate( savedInstanceState );
         setContentView(R.layout.camera_ui);
+    	System.out.println("CameraUIActivity.onCreate(): Created");
         
-        Camera cam = getCameraInstance();
+        mCamera = getCameraInstance();
+        initCamera( mCamera );
         
+        camPreview = new CameraPreview(this, mCamera);
+        previewFrame = (FrameLayout) findViewById(R.id.camera_preview);
+        previewFrame.addView(camPreview);
         
     }
     
+    /** Re-initializes camera, preview, if necessary, and resumes preview */
+    public void onResume( ){
+    	super.onResume();    	
+    	setContentView(R.layout.camera_ui);
+    	System.out.println("CameraUIActivity.onResume(): Resuming");
+        
+    	if ( null == mCamera ){
+            mCamera = getCameraInstance();
+            initCamera( mCamera );
+    	}
+    	else{
+    		try{
+    			mCamera.reconnect();
+    	    	System.out.println("CameraUIActivity.onResume(): Successfully acquired camera");
+    		}
+    		catch (Exception e){
+    			System.err.println("CameraUIActivity.onResume(): Could not access camera. Stack Trace:");
+    			e.printStackTrace();
+    			System.exit(0);
+    		}
+    	}
+        
+    	camPreview = new CameraPreview(this, mCamera);
+    	
+    	previewFrame = (FrameLayout) findViewById(R.id.camera_preview);
+	    previewFrame.addView(camPreview);
+    }
+    
+    /** Re-initializes camera, preview, if necessary, and resumes preview */
+    public void onRestart( ){
+    	super.onRestart();
+    	setContentView(R.layout.camera_ui);
+    	System.out.println("CameraUIActivity.onRestart(): Restarting");
+        
+    	if ( null == mCamera ){
+            mCamera = getCameraInstance();
+            initCamera( mCamera );
+    	}
+    	else{
+    		try{
+    			mCamera.reconnect();
+    	    	System.out.println("CameraUIActivity.onRestart(): Successfully acquired camera");
+    		}
+    		catch (Exception e){
+    			System.err.println("CameraUIActivity.onRestart(): Could not access camera. Stack Trace:");
+    			e.printStackTrace();
+    			System.exit(0);    			
+    		}
+    	}
+        
+    	camPreview = new CameraPreview(this, mCamera);
+    	
+    	previewFrame = (FrameLayout) findViewById(R.id.camera_preview);
+	    previewFrame.addView(camPreview);
+    }
+    
+    
+    /** Releases camera so other apps can use while paused */
+    public void onPause(){
+    	super.onPause();
+    	System.out.println("CameraUIActivity.onPause(): paused, releasing camera");
+    	mCamera.release();
+    }
+    
+
+    /** Releases camera so other apps can use after stopping */
+    public void onStop(){
+    	super.onStop();
+    	System.out.println("CameraUIActivity.onStop(): stopped, releasing camera, nulling member vars");
+    	mCamera.release();
+    	mCamera = null;
+    	camPreview = null;
+    	previewFrame = null;
+    }
+
+    /** Releases camera so other apps can use after being destroyed */
+    public void onDestroy(){
+    	super.onDestroy();
+    	System.out.println("CameraUIActivity.onDestroy(): destroyed, releasing camera, nulling member vars");
+    	if (mCamera != null ){
+	    	mCamera.release();
+    	}
+    }
+    
+    
     public void onClick(View view){
-    	System.out.println("Click Detected");
+    	System.out.println("CameraUIActivity.onClick(): Click Detected");
     }
     
     /** safely access an instance of the camera */
@@ -28,11 +123,20 @@ public class CameraUIActivity extends Activity implements OnClickListener{
     	
     	try{
     		cam = Camera.open();
+    		System.out.println("CameraUIActivity.getCameraInstance(): Camera found, opening");
     	}
     	catch ( Exception e ){
-    		System.out.println("No camera found");
+    		System.err.println("CameraUIActivity.getCameraInstance(): No camera found. Stack trace:");
+    		e.printStackTrace();
+    		System.exit(0);
     	}
     	
     	return cam;
+    }
+    
+    /** Initialize camera and begin recording */
+    public void initCamera( Camera cam ){
+    	cam.setDisplayOrientation(90); // rotate to portrait mode
+    	System.out.println("CameraUIActivity.initCamera(): Camera initialized");
     }
 }

a4bad060854662129c37ad57b14735521193d52d
cjm571@gmail.com <cjm571@gmail.com@c399bc76-a552-f8f4-f1ce-ebdec458d828>
2012-04-05 23:57:06 +0000

    Implemented some PreviewCallback goodness. Fixed various FC issues

--- trunk/src/com/mcallister/opencventure/CameraPreview.java
+++ trunk/src/com/mcallister/opencventure/CameraPreview.java
@@ -1,72 +1,55 @@
 package com.mcallister.opencventure;
 
 import java.io.IOException;
+
 import android.content.Context;
 import android.hardware.Camera;
+import android.hardware.Camera.PreviewCallback;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 
-
-/** A basic Camera preview class */
-public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
     private SurfaceHolder mHolder;
     private Camera mCamera;
-    String TAG = "CameraPreview";
+	
+    private static final String TAG = "Preview";
 
     public CameraPreview(Context context, Camera camera) {
         super(context);
+
         mCamera = camera;
 
         // Install a SurfaceHolder.Callback so we get notified when the
         // underlying surface is created and destroyed.
         mHolder = getHolder();
         mHolder.addCallback(this);
-        // deprecated setting, but required on Android versions prior to 3.0
         mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
     }
 
-    public void surfaceCreated(SurfaceHolder holder) {
-        // The Surface has been created, now tell the camera where to draw the preview.
-        try {
+    /** Handles changes to the preview when changed */
+	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+    	// set up preview callback
+    	mCamera.setPreviewCallback(new PreviewCallback(){
+    	    public void onPreviewFrame(byte[] data, Camera camera){
+    	    	Log.i(TAG, "onPreviewFrame called!");
+    	    }
+    	});
+	}
+
+	/** Tells the camera where to draw the preview when created */
+	public void surfaceCreated(SurfaceHolder holder) {		
+		try {
             mCamera.setPreviewDisplay(holder);
             mCamera.startPreview();
         } catch (IOException e) {
             Log.d(TAG, "Error setting camera preview: " + e.getMessage());
-        }
-    }
+        }		
+	}
 
-    public void surfaceDestroyed(SurfaceHolder holder) {
-        // empty. Take care of releasing the Camera preview in your activity.
-    }
-
-    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
-        // If your preview can change or rotate, take care of those events here.
-        // Make sure to stop the preview before resizing or reformatting it.
-
-        if (mHolder.getSurface() == null){
-          // preview surface does not exist
-          return;
-        }
-
-        // stop preview before making changes
-        try {
-            mCamera.stopPreview();
-        } catch (Exception e){
-          // ignore: tried to stop a non-existent preview
-        }
-
-        // set preview size and make any resize, rotate or
-        // reformatting changes here
-
-        // start preview with new settings
-        try {
-            mCamera.setPreviewDisplay(mHolder);
-            mCamera.startPreview();
-
-        } catch (Exception e){
-            Log.d(TAG, "Error starting camera preview: " + e.getMessage());
-        }
-    }
+	/** IGNORED. Activity will handle cleanup */
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		// ignored
+	}    
 }
--- trunk/src/com/mcallister/opencventure/CameraUIActivity.java
+++ trunk/src/com/mcallister/opencventure/CameraUIActivity.java
@@ -1,30 +1,18 @@
 package com.mcallister.opencventure;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
 import android.app.Activity;
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
-import android.hardware.Camera.PictureCallback;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
-import android.view.View;
-import android.widget.Button;
 import android.widget.FrameLayout;
-import android.widget.ImageView;
 
 public class CameraUIActivity extends Activity{
-	private Camera mCamera = null;
+	private Camera mCamera;
+	private CameraPreview mPreview;
 	
-	private CameraPreview camPreview = null;
-	private FrameLayout previewFrame = null;
-	
-	private PictureCallback mPicture = null;
-	private File mPictureFile = null;
-	private String TAG = "CameraUIActvity";
+	private String TAG = "CameraUIActivity";
 	
 	/** Displays camera_ui layout when activity is created */
     @Override
@@ -32,96 +20,30 @@ public class CameraUIActivity extends Activity{
     	super.onCreate( savedInstanceState );
         setContentView(R.layout.camera_ui);
     	System.out.println("CameraUIActivity.onCreate(): Created");
-    }
-    
-    /** Initializes camera, preview, if necessary, and resumes preview */
-    public void onResume( ){
-    	super.onResume();    	
-    	setContentView(R.layout.camera_ui);
-    	System.out.println("CameraUIActivity.onResume(): Resuming");
-        
-    	if ( null == mCamera ){
-            mCamera = getCameraInstance();
-            initCamera( mCamera );
-    	}
-    	else{
-    		try{
-    			mCamera.reconnect();
-    	    	System.out.println("CameraUIActivity.onResume(): Successfully acquired camera");
-    		}
-    		catch (Exception e){
-    			System.err.println("CameraUIActivity.onResume(): Could not access camera. Stack Trace:");
-    			e.printStackTrace();
-    			System.exit(0);
-    		}
-    	}
-        
-    	// create button and set respective onClickListener
-        Button takePictureButton = (Button) findViewById(R.id.takePictureButton);
-        takePictureButton.setOnClickListener(
-	        new View.OnClickListener() {
-	            public void onClick(View v) {
-	                // get an image from the camera
-	                mCamera.takePicture(null, null, mPicture);
-	            }
-	        }
-	    );
+    	
+    	// acquire camera and initialize params
+    	if ( !safeCameraOpen() ){
+    		Log.e(TAG, "Failed to acquire camera");
+    		System.exit(0); 
+    	}  	
+    	
+    	initCamera();  
     	
     	// create preview
-    	camPreview = new CameraPreview(this, mCamera);
-        previewFrame = (FrameLayout) findViewById(R.id.camera_preview);
-        previewFrame.addView(camPreview);
-            	
-    	mPicture = new PictureCallback() {
-    		public void onPictureTaken(byte[] imageData, Camera camera) {
-    			Log.i(TAG, "onPictureTaken() started");
-    	        mPictureFile = ImageProcessing.getImageOutputFile(1); // 1 = MEDIA_TYPE_IMAGE
-    	            	        
-    	        try{
-    	        	mPictureFile.createNewFile();
-	    	        FileOutputStream fos = new FileOutputStream(mPictureFile);
-	    	        fos.write(imageData);
-	    	        fos.close();
-	    	        Log.i(TAG, "Picture written to SD");
-    	        }
-    	        catch( IOException e )
-    	        {
-    	        	Log.e(TAG, "Failed to write image data to SD");
-    	        }
-    		}
-    	};
-    	
-    	/*
-    	ImageView imageView = (ImageView) findViewById(R.id.imageView);
-    	
-    	// Capture and process images
-		mCamera.takePicture(null, null, mPicture);
-    	ImageProcessing.ProcessImage(mPicture);
-    	//imageView.setImageURI(Uri.fromFile(mPictureFile));
-    	*/
+    	mPreview = new CameraPreview(this, mCamera);
+    	FrameLayout preview = (FrameLayout) findViewById(R.id.preview_frame);
+    	preview.addView(mPreview);
     }
     
-    
-    /** Releases camera so other apps can use while paused */
-    public void onPause(){
-    	super.onPause();
-    	System.out.println("CameraUIActivity.onPause(): paused, releasing camera");
-    	mCamera.release();
-    }
-    
-
-    /** Releases camera so other apps can use after stopping */
     public void onStop(){
     	super.onStop();
-    	System.out.println("CameraUIActivity.onStop(): stopped, releasing camera, nulling member vars");
-    	mCamera.release();
-    }
-
-    /** Releases camera so other apps can use after being destroyed */
-    public void onDestroy(){
-    	super.onDestroy();
-    	System.out.println("CameraUIActivity.onDestroy(): destroyed, releasing camera, nulling member vars");
-	    mCamera.release();
+    	
+    	// safely release camera
+		if (mCamera != null) {
+			mCamera.setPreviewCallback(null);
+			mCamera.release();
+			mCamera = null;
+		}
     }
     
     public boolean onTouchEvent(MotionEvent event){
@@ -131,31 +53,26 @@ public class CameraUIActivity extends Activity{
     }
     
     /** safely access an instance of the camera */
-    public static Camera getCameraInstance(){
-    	Camera cam = null;
-    	
-    	try{
-    		cam = Camera.open();
-    		System.out.println("CameraUIActivity.getCameraInstance(): Camera found, opening");
-    	}
-    	catch ( Exception e ){
-    		System.err.println("CameraUIActivity.getCameraInstance(): No camera found. Stack trace:");
-    		e.printStackTrace();
-    		System.exit(0);
-    	}
-    	
-    	return cam;
+    private boolean safeCameraOpen() {
+        boolean qOpened = false;
+      
+        try {
+            mCamera = Camera.open();
+            qOpened = (mCamera != null);
+        } catch (Exception e) {
+            Log.e(getString(R.string.app_name), "failed to open Camera");
+            e.printStackTrace();
+        }
+
+        return qOpened;    
     }
     
     /** Initialize camera and begin recording */
-    public void initCamera( Camera cam ){
-    	int JPEG = 0x100;
+    public void initCamera( ){
+    	Parameters camParams = mCamera.getParameters();
     	
-    	Parameters camParams = cam.getParameters();
-    	camParams.setPictureFormat(JPEG);
-    	cam.setParameters(camParams);
-    	
-    	cam.setDisplayOrientation(90); // rotate to portrait mode
+    	camParams.setRotation(90);
+    	mCamera.setParameters(camParams);
     	
     	System.out.println("CameraUIActivity.initCamera(): Camera initialized");
     }

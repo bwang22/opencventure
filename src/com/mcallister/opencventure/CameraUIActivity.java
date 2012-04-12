bab8d65658b3f00a68bc0a156446dc82f5e19d02
cjm571@gmail.com <cjm571@gmail.com@c399bc76-a552-f8f4-f1ce-ebdec458d828>
2012-04-12 06:21:35 +0000

    Removed all that wishy-washy CameraPreview garbage. Replaced with proper OpenCV VideoCapture implementation. Basics of threading started

--- src/com/mcallister/opencventure/CameraUIActivity.java
+++ src/com/mcallister/opencventure/CameraUIActivity.java
@@ -1,15 +1,11 @@
 package com.mcallister.opencventure;
 
 import android.app.Activity;
-import android.hardware.Camera;
-import android.hardware.Camera.Parameters;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.FrameLayout;
 
 public class CameraUIActivity extends Activity{
-	private Camera mCamera;
-	private CameraPreview mPreview;
 	private DrawingView mDrawingView;
 	
 	private String TAG = "CameraUIActivity";
@@ -21,56 +17,14 @@ public class CameraUIActivity extends Activity{
         setContentView(R.layout.camera_ui);
     	Log.i(TAG, "Created");
     	
-    	// acquire camera and initialize params
-    	if ( !safeCameraOpen() ){
-    		Log.e(TAG, "Failed to acquire camera");
-    		System.exit(0); 
-    	}  	
-    	
-    	initCamera();  
-    	
     	// create preview
-    	mPreview = new CameraPreview(this, mCamera);
     	mDrawingView = new DrawingView(this);
     	FrameLayout preview = (FrameLayout) findViewById(R.id.preview_frame);
-    	preview.addView(mPreview);
     	preview.addView(mDrawingView);
     }
     
     public void onStop(){
     	super.onStop();
     	Log.i(TAG, "Stopping");
-    	
-    	// safely release camera
-		if (mCamera != null) {
-			mCamera.setPreviewCallback(null);
-			mCamera.release();
-			mCamera = null;
-		}
-    }
-    
-    /** safely access an instance of the camera */
-    private boolean safeCameraOpen() {
-        boolean qOpened = false;
-      
-        try {
-            mCamera = Camera.open();
-            qOpened = (mCamera != null);
-        } catch (Exception e) {
-            Log.e(getString(R.string.app_name), "failed to open Camera");
-            e.printStackTrace();
-        }
-
-        return qOpened;    
-    }
-    
-    /** Initialize camera and begin recording */
-    public void initCamera( ){
-    	Parameters camParams = mCamera.getParameters();
-    	
-    	camParams.setRotation(90);
-    	mCamera.setParameters(camParams);
-    	
-    	Log.i(TAG, "Camera initialized");
     }
 }

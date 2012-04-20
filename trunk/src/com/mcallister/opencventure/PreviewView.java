1266a4e7baa4f7b4c82e4dee638ef4e613c49552
cjm571@gmail.com <cjm571@gmail.com@c399bc76-a552-f8f4-f1ce-ebdec458d828>
2012-04-20 00:23:29 +0000

    Performance Increase! :D Dropped jpeg compression, replaced with direct conversion using OpenCV matrices

--- trunk/src/com/mcallister/opencventure/PreviewView.java
+++ trunk/src/com/mcallister/opencventure/PreviewView.java
@@ -1,14 +1,13 @@
 package com.mcallister.opencventure;
-
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.ImageFormat;
-import android.graphics.Rect;
-import android.graphics.YuvImage;
 import android.hardware.Camera;
 import android.hardware.Camera.PreviewCallback;
 import android.hardware.Camera.Size;
@@ -22,7 +21,7 @@ public class PreviewView extends SurfaceView implements SurfaceHolder.Callback {
     private Camera mCamera;
     private BitmapFactory.Options mBmpOpts;
     
-    private String TAG = "CameraPreview";
+    private String TAG = "PreviewView";
 
     public PreviewView(Context context, Camera camera) {
         super(context);
@@ -35,6 +34,7 @@ public class PreviewView extends SurfaceView implements SurfaceHolder.Callback {
         mHolder.addCallback(this);
         mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
     }
+    
 
     /** Handles changes to the preview when changed */
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
@@ -53,16 +53,14 @@ public class PreviewView extends SurfaceView implements SurfaceHolder.Callback {
 	    mCamera.setPreviewCallback(new PreviewCallback(){
 	        public void onPreviewFrame(byte[] data, Camera camera){
 	            //Log.i(TAG, "onPreviewFrame called!");
+	        	Size previewSize = camera.getParameters().getPreviewSize();
 	            
-	            // Convert to JPG
-	            Size previewSize = camera.getParameters().getPreviewSize(); 
-	            YuvImage yuvimage=new YuvImage(data, ImageFormat.NV21, previewSize.width, previewSize.height, null);
-	            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-	            yuvimage.compressToJpeg(new Rect(0, 0, previewSize.width, previewSize.height), 100, baos);
-	            byte[] jdata = baos.toByteArray();
-
-	            // Convert to Bitmap
-	            CameraUIActivity.sharedBmp = BitmapFactory.decodeByteArray(jdata, 0, jdata.length);
+	        	// construct matrix from YUV420 data and convert to RGB
+	            Mat yuvMat = new Mat(previewSize.height + previewSize.height / 2, previewSize.width, CvType.CV_8UC1);
+	            yuvMat.put(0, 0, data);
+	            Mat rgbaMat = new Mat();
+	            Imgproc.cvtColor(yuvMat, rgbaMat, Imgproc.COLOR_YUV420sp2RGB, 4);
+	            CameraUIActivity.sharedMat = rgbaMat;
 	        }
 	    });
 	}
